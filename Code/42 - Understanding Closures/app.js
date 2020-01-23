878cf29242582b798324861b5adcd3d9b3ac2465
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-01-23 12:11:14 -0600

    42 - Understanding Closures

---
+++ Code/42 - Understanding Closures/app.js
@@ -0,0 +1,10 @@
+function greet(whattosay) {
+    return function(name) {
+        console.log(whattosay + ' ' + name);
+    }
+}
+
+//greet('Hi')('Tony');
+
+var sayHi = greet('Hi');
+sayHi('Tony');
