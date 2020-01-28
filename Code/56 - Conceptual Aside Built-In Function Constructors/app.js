50e610460c3665dcad0e76fc8652c942f79e626c
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-01-28 11:24:09 -0600

    56 - Conceptual Aside Built-In Function Constructors

---
+++ Code/56 - Conceptual Aside Built-In Function Constructors/app.js
@@ -0,0 +1,12 @@
+String.prototype.isLengthGreaterThan = function(limit) {
+    return this.length > limit;
+}
+
+console.log("John".isLengthGreaterThan(3));
+
+Number.prototype.isPositive = function() {
+    return this > 0;
+}
+
+var a  = Number(3) 
+console.log(a.isPositive());
