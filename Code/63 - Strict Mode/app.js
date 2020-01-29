7e6050640f4ab2fe78b1b22831e0ad3762883eca
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-01-29 11:39:32 -0600

    63 - Strict Mode

---
+++ Code/63 - Strict Mode/app.js
@@ -0,0 +1,16 @@
+// can use at the top of file
+"use strict";
+
+function logNewPerson() {
+    // can use within the execution context of a function only
+    //"use strict";
+
+    var person2;
+    persom2 = {}
+    console.log(persom2);
+}
+
+var person;
+persom = {};
+console.log(persom);
+logNewPerson();
