de77d12498c2df4e191515671fbf8531ad7719ca
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-01-30 12:29:48 -0600

    70 - Our Object And Prototype

---
+++ Code/70 - Our Object And Prototype/Greetr.js
@@ -0,0 +1,22 @@
+(function greetr(global, $) {
+
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
+
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language) {
+        
+        var self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = firstName || 'en';
+
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
